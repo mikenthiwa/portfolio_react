fda144192a0e1991c82c92a88de3785db1cdba32
mikenthiwa <mikenthiwa@gmail.com>
2020-09-04 18:21:26 +0300

    Ch(upadate class component to functional):  - Update home component.  - Update navbar component.  - Update description component. WIP

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,45 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faCopyright } from '@fortawesome/free-solid-svg-icons';
 import FontAwesomeComponent from "../../helper";
 import './footer.scss';
 
-class Footer extends Component{
-    render() {
+const socialIcons = [
+    ['http://linkedin.com/in/michaelnthiwamutua/mynetwork', faLinkedin],
+    ['https://github.com/mikenthiwa', faGithub],
+    ['https://twitter.com/Michael19289994', faTwitter]
+];
+
+const Footer = () => {
+
+    const renderSocialIcons = () => {
+        return socialIcons.map((icon, index ) => {
+            return (
+                <FontAwesomeComponent icon={icon[1]} color='white' size='2x' link={icon[0]} key={index} />
+            )
+        })
+    };
+
+    const renderCopyrightIcon = () => {
         return (
-            <div className="footer-cont">
-                <div className="icon-cont">
-                    <FontAwesomeComponent
-                        icon={faLinkedin}
-                        color='white'
-                        size='2x'
-                        link='http://linkedin.com/in/michaelnthiwamutua/mynetwork' />
-                    <FontAwesomeComponent
-                        icon={faGithub}
-                        color='white'
-                        size='2x'
-                        link='https://github.com/mikenthiwa'
-                    />
-                    <FontAwesomeComponent
-                        icon={faTwitter}
-                        color='white'
-                        size='2x'
-                        link='https://twitter.com/Michael19289994'
-                    />
-                </div>
-                <div className='copyright-cont'>
-                    <div className='copyright-icon'>
-                        <FontAwesomeComponent
-                            icon={faCopyright}
-                            color='white'
-                            size='1x'
-                        />
-                    </div>
-                    mike.nthiwa@gmail.com
-                </div>
+            <div className='copyright-icon'>
+                <FontAwesomeComponent icon={faCopyright} color='white' size='1x' />
             </div>
-        );
+        )
     }
+
+    return (
+        <div className="footer-cont">
+            <div className="icon-cont">
+                {renderSocialIcons()}
+            </div>
+            <div className='copyright-cont'>
+                {renderCopyrightIcon()}
+                mike.nthiwa@gmail.com
+            </div>
+        </div>
+    );
 }
 
-export default Footer
+export default Footer;
